2dfe1396febe4973a8bc292d70fb1bc9a36ea00c
Santiago Cuervo <cuervo.santiago@gmail.com>
2024-09-14 12:02:28 -0500

    business-layer.py: Add business layer implementation

--- business-layer.py
+++ business-layer.py
@@ -1,35 +1,35 @@
 class BusinessLayer:
     def __init__(self, data_layer):
-        self.data_layer = data_layer
+        self.data_layer = data_layer # Iniciación instancia capa de datos
 
     def add_customer(self, name, pet_name):
-        customer = {
+        customer = { # Crear diccionario customer
             'name': name,
             'pet_name': pet_name
         }
-        self.data_layer.add_customer(customer)
+        self.data_layer.add_customer(customer) # Guardar customer en capa de datos
 
     def add_service(self, service_name, price):
-        service = {
+        service = { # Crear diccionario service
             'service_name': service_name,
             'price': price
         }
-        self.data_layer.add_service(service)
+        self.data_layer.add_service(service) # Guardar service en capa de datos
 
     def get_customers(self):
-        return self.data_layer.get_customers()
+        return self.data_layer.get_customers() # Ver lista customers mediante método de capa de datos
 
     def get_services(self):
-        return self.data_layer.get_services()
+        return self.data_layer.get_services() # Ver lista services mediante método de capa de datos
 
-    def book_service(self, customer_name, service_name):
-        customers = self.data_layer.get_customers()
-        services = self.data_layer.get_services()
+    def book_service(self, customer_name, service_name): # Agendar servicio
+        customers = self.data_layer.get_customers() # Incializar referencia a customers de capa de datos
+        services = self.data_layer.get_services() # Inicializer referencia a services de capa de datos
 
-        customer = next((c for c in customers if c['name'] == customer_name), None)
-        service = next((s for s in services if s['service_name'] == service_name), None)
+        customer = next((customer for customer in customers if c['name'] == customer_name), None) # Verificación de existencia del cliente
+        service = next((service for service in services if s['service_name'] == service_name), None) # Verificación de existencia del servicio
 
-        if customer and service:
-            return f"{customer['pet_name']} has been booked for {service_name} service."
+        if customer and service: # Si ya se creó el cliente y el servicio...
+            return f" Se ha agendado el servicio \"{service_name}\" para {customer['pet_name']}." # Agendamiento del servicio
         else:
-            return "Customer or service not found."
+                return "No se ha encontrado el cliente o el servicio indicado." # No se agenda el servicio
